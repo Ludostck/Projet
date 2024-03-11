f663cca7e6418984f65c3010dc540ddfccae3b77
Ludovic <ludostck@gmail.com>
2024-03-11 17:31:01 +0100

    fin

--- inter.py
+++ inter.py
@@ -1,33 +1,24 @@
 import os
 from PIL import Image
 
-# Chemins des dossiers
 input_directory = 'Interpol'
 output_directory = 'Interpols'
 
-# Facteur d'interpolation
 alpha = 0.5
 
-# Création du dossier de destination s'il n'existe pas
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
-# Obtenir la liste de tous les fichiers image dans le dossier source
-images = [f for f in os.listdir(input_directory) if f.endswith('.jpg') or f.endswith('.png')]  # Ajoutez d'autres formats si nécessaire
-images.sort()  # Assurez-vous que les images sont triées si leur ordre est important
+images = [f for f in os.listdir(input_directory) if f.endswith('.jpg') or f.endswith('.png')] 
+images.sort() 
 
-# Interpoler les images deux à deux
-for i in range(len(images) - 1):  # -1 car on prend des paires
-    # Ouvrir les images
+for i in range(len(images) - 1): 
     image1 = Image.open(os.path.join(input_directory, images[i])).convert('RGBA')
     image2 = Image.open(os.path.join(input_directory, images[i+1])).convert('RGBA')
 
-    # Assurer que les deux images ont la même taille
     image2 = image2.resize(image1.size, Image.LANCZOS)
 
-    # Créer l'image interpolée
     interpolated_image = Image.blend(image1, image2, alpha)
 
-    # Construire le chemin de sortie et sauvegarder l'image interpolée
     output_path = os.path.join(output_directory, f'interpolated_{i}.png')
     interpolated_image.save(output_path)
--- modfi.py
+++ modfi.py
@@ -2,10 +2,8 @@ import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 from tensorflow.keras.preprocessing.image import save_img
 
-# Chemins des dossiers
 input_directory = "Images_HD/Lunes/validation"
 output_directory = "Images_HD/Lunes/validation_bis"
-# Création du dossier de destination s'il n'existe pas
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
@@ -23,17 +21,14 @@ train_datagen = ImageDataGenerator(
     
 )
 
-# Parcourir toutes les images dans le dossier d'entrée
 for filename in os.listdir(input_directory):
-    if filename.endswith(".jpg") or filename.endswith(".png"):  # Ajoutez plus de formats si nécessaire
-        # Chargement de l'image
+    if filename.endswith(".jpg") or filename.endswith(".png"):  
         img = load_img(os.path.join(input_directory, filename))
         x = img_to_array(img)
         x = x.reshape((1,) + x.shape)
 
-        # Générer des images transformées
         i = 0
         for batch in train_datagen.flow(x, batch_size=1, save_to_dir=output_directory, save_prefix='aug', save_format='jpeg'):
             i += 1
-            if i >= 1:  # Changez ce nombre pour générer plus d'images par fichier
-                break  # Sinon, le générateur bouclerait indéfiniment
+            if i >= 1: 
+                break  
